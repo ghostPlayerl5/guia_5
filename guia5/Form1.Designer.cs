bd9efb294de8f3806a4a2b5e2773638e76402886
Ivan Reyes <ivinreyes1@gmail.com>
2021-02-12 20:53:46 -0600

    first commit

---
+++ guia5/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace guia5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.salarytxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gerntbuttom = new System.Windows.Forms.RadioButton();
+            this.subGbuttom = new System.Windows.Forms.RadioButton();
+            this.secretbuttom = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULADOR DE SUELDO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(75, 173);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(167, 22);
+            this.nametxt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NOMBRE";
+            // 
+            // salarytxt
+            // 
+            this.salarytxt.Location = new System.Drawing.Point(75, 284);
+            this.salarytxt.Name = "salarytxt";
+            this.salarytxt.Size = new System.Drawing.Size(167, 22);
+            this.salarytxt.TabIndex = 3;
+            this.salarytxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SALARIO";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(237, 435);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // gerntbuttom
+            // 
+            this.gerntbuttom.AutoSize = true;
+            this.gerntbuttom.Location = new System.Drawing.Point(379, 173);
+            this.gerntbuttom.Name = "gerntbuttom";
+            this.gerntbuttom.Size = new System.Drawing.Size(81, 21);
+            this.gerntbuttom.TabIndex = 7;
+            this.gerntbuttom.TabStop = true;
+            this.gerntbuttom.Text = "Gerente";
+            this.gerntbuttom.UseVisualStyleBackColor = true;
+            // 
+            // subGbuttom
+            // 
+            this.subGbuttom.AutoSize = true;
+            this.subGbuttom.Location = new System.Drawing.Point(379, 235);
+            this.subGbuttom.Name = "subGbuttom";
+            this.subGbuttom.Size = new System.Drawing.Size(106, 21);
+            this.subGbuttom.TabIndex = 8;
+            this.subGbuttom.TabStop = true;
+            this.subGbuttom.Text = "SubGerente";
+            this.subGbuttom.UseVisualStyleBackColor = true;
+            // 
+            // secretbuttom
+            // 
+            this.secretbuttom.AutoSize = true;
+            this.secretbuttom.Location = new System.Drawing.Point(379, 285);
+            this.secretbuttom.Name = "secretbuttom";
+            this.secretbuttom.Size = new System.Drawing.Size(94, 21);
+            this.secretbuttom.TabIndex = 9;
+            this.secretbuttom.TabStop = true;
+            this.secretbuttom.Text = "Secretaria";
+            this.secretbuttom.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(237, 361);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 527);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.secretbuttom);
+            this.Controls.Add(this.subGbuttom);
+            this.Controls.Add(this.gerntbuttom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.salarytxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nametxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox salarytxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton gerntbuttom;
+        private System.Windows.Forms.RadioButton subGbuttom;
+        private System.Windows.Forms.RadioButton secretbuttom;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ guia5/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guia5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            label5.Text = "";
+            salarytxt.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double salary = Convert.ToDouble(salarytxt.Text);
+            if (gerntbuttom.Checked)
+            {
+                double R1 = salary * 0.20;
+                double RF = salary - R1;
+                label4.Text = "sueldo Liquido: " + RF.ToString();
+                label5.Text = "Gerente: " + nametxt.Text + "\nsalario: " + salary + "\ndescontado: " + R1;
+            }
+            else if (subGbuttom.Checked)
+            {
+                double R2 = salary * 0.15;
+                double RF = salary - R2;
+                label4.Text = "Sueldo liquido: " +RF.ToString();
+                label5.Text = "SubGerente: " + nametxt.Text + "\nsalario:" + salary + "\ndescontado: " + R2;
+            }
+            else if (secretbuttom.Checked)
+            {
+                double R3 = salary * 0.05;
+                double RF = salary - R3;
+                label4.Text = "sueldo liquido: " + RF.ToString();
+                label5.Text = "Secretaria: " + nametxt.Text + "\nsalario: " + salary +"\ndescontado: " + R3;
+            }
+                        
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
